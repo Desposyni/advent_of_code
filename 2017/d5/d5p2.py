7bd22b7e87f93a9977c031fbb0d4e4b98bea0a25
Billy Nix <nix.billy@gmail.com>
2025-07-03 23:14:34 +0200

    Solve 2017 day 5

---
+++ 2017/d5/d5p2.py
@@ -0,0 +1,12 @@
+with open('d5p1.in') as f:
+    offsets = f.read().split('\n')
+offsets = [int(offset) for offset in offsets]
+
+i = 0
+step = 0
+while 0 <= i < len(offsets):
+    step += 1
+    print(step)
+    next_i = i + offsets[i]
+    offsets[i] += -1 if offsets[i] >=3 else 1
+    i = next_i
