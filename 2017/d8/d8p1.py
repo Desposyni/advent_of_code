090016ec166c19f9af188b6852ae594d53fe8354
Billy Nix <nix.billy@gmail.com>
2025-08-06 20:27:48 +0200

    Solve 2017 day 8 part 1

---
+++ 2017/d8/d8p1.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from re import match
+
+with open('d8p1.in') as f:
+    instructs = f.read().split('\n')
+
+registers = defaultdict(int)
+instruct_regex = r'(\w+) (inc|dec) (-?\d+) if (\w+) (\W+) (-?\d+)'
+
+for instruct in instructs:
+    reg, mod, amt, reg_chk, mod_chk, amt_chk = match(instruct_regex, instruct).groups()
+    amt, amt_chk = int(amt), int(amt_chk)
+
+    if eval(f'registers["{reg_chk}"] {mod_chk} {amt_chk}'):
+        registers[reg] += amt if mod == 'inc' else amt * -1
+
+print(max(registers.values()))
