b7becbb8d29f509c8d7ae15e8f421f9e938831c6
Billy Nix <nix.billy@gmail.com>
2025-08-31 16:29:55 +0200

    Solve 2019 day 1 part 2

--- 2019/d1/d1p2.py
+++ 2019/d1/d1p2.py
@@ -1,4 +1,17 @@
 with open('d1p1.in') as f:
-    modules = [int(module) for module in f.read().split('\n')]
+    modules: list[int] = [int(module) for module in f.read().split('\n')]
 
-fuels = [module // 3 - 2 for module in modules]
+def calc_fuel(mass: int) -> int:
+    return mass // 3 - 2 if mass > 8 else 0
+
+fuels: list[int] = [calc_fuel(module) for module in modules]
+module_fuels = sum(fuels)
+
+fuel_fuel = 0
+for fuel in fuels:
+    while extra := calc_fuel(fuel):
+        fuel_fuel += extra
+        fuel = extra
+
+all_fuel = module_fuels + fuel_fuel
+print(f'{all_fuel = }')
