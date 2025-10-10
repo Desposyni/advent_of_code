d9eb4face287578d41a02c086453fa8c75198e4a
Billy Nix <nix.billy@gmail.com>
2025-10-11 01:59:04 +0200

    Solve 2016 day 7 part 2

---
+++ 2016/d7/d7p2.py
@@ -0,0 +1,15 @@
+from re import findall
+
+with open('d7p1.in') as f:
+    addresses = f.read().split('\n')
+
+ssl_addresses = []
+
+for address in addresses:
+    abas = findall(r'(?=(\w)(?!\1)(\w)\1(?!\w*?]))', address)
+    for a, b in abas:
+        bab = findall(rf'{b}{a}{b}(?=\w*?])', address)
+        if bab:
+            ssl_addresses.append(address)
+            break
+print(len(ssl_addresses))
